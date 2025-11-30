cb2927db376c8e6d8f522c4e4c18278bd88aaa1d
Alan Lengkoan <alanlengkoan15@gmail.com>
2025-11-30 11:41:32 +0800

    update again

--- shopify/tailwind.config.js
+++ shopify/tailwind.config.js
@@ -14,6 +14,13 @@ export default {
         'flores-light': '#9AD7E5',
         'flores-footer': '#143F58',
         'flores-separator': '#E2F4F7',
+        'sustainability': {
+          'bg': '#e6f0f2',
+          'text': '#1f4254',
+          'desc': '#476472',
+          'btn': '#2f5b6b',
+          'btn-hover': '#244955',
+        },
       },
       fontFamily: {
         'heading': ['Cormorant Garamond', 'serif'],
