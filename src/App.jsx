b551b00e2994b1b159916f89c7360743988c3586
Alan Lengkoan <alanlengkoan15@gmail.com>
2025-11-30 08:57:21 +0800

    update template

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { RouterProvider } from "react-router";
-import { HelmetProvider } from 'react-helmet-async';
 import { CartProvider } from './context/CartContext';
 import router from "./route";
 
 function App() {
   return (
-    <HelmetProvider>
-      <CartProvider>
+    <CartProvider>
         <RouterProvider router={router} />
-      </CartProvider>
-    </HelmetProvider>
+    </CartProvider>
   );
 }
 
--- src/components/SEOHead.jsx
+++ src/components/SEOHead.jsx
@@ -1,4 +1,4 @@
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from 'react-helmet';
 
 function SEOHead({
   title = "Flores Island Coffee - Premium Indonesian Coffee Beans Export",
